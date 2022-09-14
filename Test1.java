bde9ed9d28dac67b3a3595f11b2f7cfafe77adc2
azmal <syedmohdazmal@gmail.com>
2022-09-14 12:42:13 +0530

    modified commit first

--- Test1.java
+++ Test1.java
@@ -3,5 +3,7 @@ class Test1
 public static void main(String args[])
 {
 Syste.out.println("Hello, this is first programs line one");
+Syste.out.println("Hello, this is first programs line two");
+Syste.out.println("Hello, this is first programs line three");
 }
 }
--- Test2.java
+++ Test2.java
@@ -2,6 +2,10 @@ class Test2
 {
 public static void main(String args[])
 {
+Syste.out.println("Hello, this is second programs line One");
 Syste.out.println("Hello, this is second programs line Two");
+Syste.out.println("Hello, this is second programs line Three");
+Syste.out.println("Hello, this is second programs line Four");
+
 }
 }
