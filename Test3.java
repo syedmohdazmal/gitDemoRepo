eb7c24d5b8729397ec9cb48c5c37c07921c0f068
syed mohammed azmal <108061158+syedmohdazmal@users.noreply.github.com>
2022-09-14 12:48:14 +0530

    Update Test3.java

--- Test3.java
+++ Test3.java
@@ -3,5 +3,13 @@ class Test3
 public static void main(String args[])
 {
 Syste.out.println("Hello, this is third programs line one");
+  Syste.out.println("Hello, this is third programs line Two");
+  Syste.out.println("Hello, this is third programs line Three");
+    Syste.out.println("Hello, this is third programs line Two");
+  Syste.out.println("Hello, this is third programs line Three");
+    Syste.out.println("Hello, this is third programs line helloooooo");
+  Syste.out.println("Hello, this is third programs line b2btesters");
+    Syste.out.println("Hello, this is third programs line value pitch");
+  Syste.out.println("Hello, this is third programs line happy birth days");
 }
 }
